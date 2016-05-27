868baa284b9dbc7bbd120e020d87d71c36a6c0f2
Max Botviniev <botvinev.maxim@gmail.com>
2016-05-27 16:24:58 +0300

    Path rebuild

--- Source/Pathes.h
+++ Source/Pathes.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
-const std::string root_path = "D:/C++/OpenGL/PixelsDefence/PixelsDefence/";
-const std::string resources_path = "D:/C++/OpenGL/PixelsDefence/PixelsDefence/Resources/";
-const std::string shaders_path = "D:/C++/OpenGL/PixelsDefence/PixelsDefence/Resources/Shaders/";
-const std::string textures_path = "D:/C++/OpenGL/PixelsDefence/PixelsDefence/Resources/Textures/";
+const std::string root_path = "D:/C++/OpenGL/PixelsDefence/";
+const std::string project_path = root_path + "PixelsDefence/";
+const std::string repo_path = project_path + "PixelsDefence_REPO/";
+const std::string resources_path = repo_path + "Resources/";
+const std::string shaders_path = resources_path + "Shaders/";
+const std::string textures_path = resources_path + "Textures/";
